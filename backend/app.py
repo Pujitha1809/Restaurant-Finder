7b8720f5ae967f7b8682f53f53a7def1097816b6
Pujitha Mamidishetty <140079940+Pujitha1809@users.noreply.github.com>
2026-04-01 18:12:56 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -34,5 +34,5 @@ def get_restaurants():
     return jsonify(restaurants)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
